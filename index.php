4407ce04950d339c0bbea48d777976f939e2f48a
root <root@jenkins-master1.asia-south1-a.c.planar-leaf-240912.internal>
2020-03-20 18:26:54 +0000

    1

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<h1>Hello Jawwy!</h1>
+<h1>Hello Jawwy!!!</h1>
 <h4>Attempting MySQL connection from php...</h4>
+<h4>Start of Portal</h4>
+
 <?php
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
+$host = 'localhost';
+$user = 'web1';
+$pass = 'Web1l234567?-';
 $conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
@@ -13,3 +15,5 @@ if ($conn->connect_error) {
 }
 
 ?>
+
+<h4>End of Portal</h4>
